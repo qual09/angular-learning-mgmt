bf8b37128bad9464e18ca72c00b855f217b4d193
HCPX <cpx.hieu@gmail.com>
2020-10-26 20:00:09 +0100

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CourseListComponent } from './components/course-list/course-list.component';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/courseList', pathMatch: 'full' },
+  { path: 'courseList', component: CourseListComponent },
+  { path: 'courseDetails/:id', component: CourseDetailsComponent },
+  { path: '**', component: CourseListComponent }
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './components/course-list/course-list.component';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    CourseDetailsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
