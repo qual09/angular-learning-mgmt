ec6b244c57c45bc5d0030da4a8c1cbd38296793b
HCPX <cpx.hieu@gmail.com>
2020-10-26 21:17:01 +0100

    added mocks

---
+++ src/app/mocks/mock-courses.ts
@@ -0,0 +1,42 @@
+import { Course } from '../models/course';
+
+export const COURSES: Course[] = [
+  {
+    "id": 1,
+    "name": "Fundamentals of Credit",
+    "imageUrl": "https://picsum.photos/100/100",
+    "status": "DRAFT",
+    "images": [
+      "https://picsum.photos/300/300",
+      "https://picsum.photos/300/300",
+      "https://picsum.photos/300/300"
+    ],
+    "instructors": [
+      {
+        "name": "Roli Jain",
+        "image": "https://picsum.photos/300/300"
+      },
+      {
+        "name": "Sebastian Taylor",
+        "image": "https://picsum.photos/300/300"
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Accounting Fundamentals",
+    "status": "PUBLISHED",
+    "imageUrl": "https://picsum.photos/100/100",
+    "images": [
+      "https://picsum.photos/300/300",
+      "https://picsum.photos/300/300",
+      "https://picsum.photos/300/300"
+    ],
+    "instructors": [
+      {
+        "name": "Roli Jain",
+        "image": "https://picsum.photos/300/300"
+      }
+    ]
+  }
+]
